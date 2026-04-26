ac0c4a5f38553940a25c19bcbfed6ad1e2be3377
justingarcia-wow <justin.garcia@utp.edu.co>
2026-04-26 18:39:12 -0500

    feat: quitar token

--- vars/buildAndPush.groovy
+++ vars/buildAndPush.groovy
@@ -8,7 +8,7 @@ def call(String imageName) {
 
     stage('Build y Push a Docker Hub') {
         sh """
-            docker login -u justing0 -p dckr_pat_Cp48FdPKfj-76paEBamKNw0v0vE
+            docker login -u justing0 -p TOKEN_ACA
             docker buildx build --platform linux/amd64 -t ${imageName}:latest --push .
         """
     }
